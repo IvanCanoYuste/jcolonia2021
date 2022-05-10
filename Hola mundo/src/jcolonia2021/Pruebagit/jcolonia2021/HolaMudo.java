c97ee6edaf2fda2c6d25af751049626026a6752a
Ivan Cano Yuste <daw1m12@IC12>
2022-05-10 10:41:43 +0200

    Experimento 1

---
+++ Hola mundo/src/jcolonia2021/Pruebagit/jcolonia2021/HolaMudo.java
@@ -0,0 +1,14 @@
+package jcolonia2021;
+
+public class HolaMudo {
+
+	public HolaMudo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+
+	}
+
+}
